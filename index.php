0ba42c4851afdaf121d9466aeddb981b40ed0e55
nyatado98 <danndong080@gmail.com>
2023-07-07 17:16:34 +0300

    index

--- index.php
+++ index.php
@@ -229,8 +229,10 @@ while($row=$query->fetch_assoc()) {
 				<?php while($row = $ress->fetch_assoc()){
 					?>
                 <li class="active">
-                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><?php echo $row['departmentName'];?></a>
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle font-weight-bold text-dark"><?php echo $row['departmentName'];?></a>
                     <ul class="collapse list-unstyled" id="homeSubmenu">
+					
+						
                         <li>
                             <a href="#">Home 1</a>
                         </li>
@@ -241,29 +243,6 @@ while($row=$query->fetch_assoc()) {
                             <a href="#">Home 3</a>
                         </li>
                     </ul>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
-                    <ul class="collapse list-unstyled" id="pageSubmenu">
-                        <li>
-                            <a href="#">Page 1</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 3</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Portfolio</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
                 </li>
 				<?php } ?>
             </ul>
