c715b0233a889b41a1247ac1da3f5e5310f82ccc
nyatado98 <danndong080@gmail.com>
2023-08-29 13:53:15 +0300

    other changes

---
+++ other_files_deleted.php
@@ -0,0 +1,55 @@
+<!-- <div class="col-md-3 bg-dark"> -->
+    				
+    				<!-- <div class="column" style="border-bottom: .1px solid white;">
+    					<div class="row dropdown mx-auto justify-content-between">
+    				<a href="#" id="dropdown" data-target="#new" data-toggle="collapse" style="color: white;text-decoration: none;font-size:20px;" class="collapsed font-weight-bold m-2">
+						<?php echo $r['departmentName'];?>
+					</a>
+					<p class="sub-menu collapse" id="new">
+                  <a href="">Sub departments</a>
+					</p>
+    				<a href="">
+    				<i class="fa fa-arrow-down mt-2" style="color:whitesmoke;font-size: 17px;"></i>
+    			</a>
+    			</div>
+    				</div> -->
+			<!-- 		<nav id="sidebar">
+            <div class="sidebar-header">
+                <h3 class="text-white font-weight-bold text-center" style="text-decoration:underline">Departments</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+				<?php while($row = $ress->fetch_assoc()){
+					?>
+                <li class="active">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle font-weight-bold text-dark"><?php echo $row['departmentName'];?></a>
+					<?php } ?>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+					
+						
+                        <li>
+                            <a href="#">Home 1</a>
+                        </li>
+                        <li>
+                            <a href="#">Home 2</a>
+                        </li>
+                        <li>
+                            <a href="#">Home 3</a>
+                        </li>
+                    </ul>
+                </li>
+				
+            </ul>
+			
+
+            <ul class="list-unstyled CTAs">
+                <li>
+                    <a href="settings" class="download">Settings</a>
+                </li>
+                <li>
+                    <a href="logout" class="article">Log out</a>
+                </li>
+            </ul>
+        </nav> -->
+
+    			<!-- </div> -->
